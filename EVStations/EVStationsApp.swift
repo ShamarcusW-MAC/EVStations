694fcab3ffb0effc8421c1906d7861034beb3c5f
ShaMarcus Walker <shamarcusw84@gmail.com>
2025-08-19 05:27:18 -0400

    Initial Commit

---
+++ EVStations/EVStationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EVStationsApp.swift
+//  EVStations
+//
+//  Created by Consultant on 8/19/25.
+//
+
+import SwiftUI
+
+@main
+struct EVStationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
