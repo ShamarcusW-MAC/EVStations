09fba8312ecba07f71854708bbfea544f4e6e0a3
ShaMarcus Walker <shamarcusw84@gmail.com>
2025-08-20 09:55:13 -0400

    Added Config file for global variables

---
+++ EVStations/Util/Config.swift
@@ -0,0 +1,15 @@
+//
+//  Config.swift
+//  EVStations
+//
+//  Created by Shamarcus Walker on 8/19/25.
+//
+
+import Foundation
+
+enum Config {
+    static let apiKey = "34e45eb6-2f5e-498b-b1a1-b42fa09a38d8"
+    static let baseURL = "https://api.openchargemap.io"
+    static let defaultDistanceKM: Double = 10
+    static let defaultMaxResults: Int = 50
+}
